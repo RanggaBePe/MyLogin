99e4e7551fc95dcc697bab1843878133ac5bf214
RanggaBePe <ranggabramantya88@gmail.com>
2018-08-22 20:24:36 +0700

    login commit

---
+++ app/src/main/java/com/example/rangga/mylogin/util/Server.java
@@ -0,0 +1,9 @@
+package com.example.rangga.mylogin.util;
+
+/**
+ * Created by RANGGA BRAMANTYA.
+ */
+
+public class Server {
+    public static final String URL = "http://sasmitoh.nitarahmawati.my.id/";
+}
